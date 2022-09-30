d83aa628e3081a1998cde0a02e5c081b2221635f
Geoffrey <g_gonzales5@u.pacific.edu>
2022-09-30 16:22:03 -0700

    pushing //p

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -10,7 +10,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage("robot.png", 200, 200);
 		add(robot);
-
+		//p
 		final GLabel label = new GLabel("mcchicken", 200, 400);
 		add(label);
 		
